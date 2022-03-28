b1fffec4c08ea192e7a1e1d205eec77d9970de95
mtnmr <mei.rila1013@gmail.com>
2022-03-28 15:29:37 +0900

    preference(saveLayout)

--- WordsApp/app/src/main/java/com/example/wordsapp/Fragment/LetterListFragment.kt
+++ WordsApp/app/src/main/java/com/example/wordsapp/Fragment/LetterListFragment.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wordsapp.LetterAdapter
 import com.example.wordsapp.R
+import com.example.wordsapp.data.SettingsDataStore
 import com.example.wordsapp.databinding.FragmentLetterListBinding
+import kotlinx.coroutines.launch
 
 
 class LetterListFragment : Fragment() {
@@ -19,6 +23,10 @@ class LetterListFragment : Fragment() {
 
     private lateinit var recyclerView:RecyclerView
     private var isLinearLayoutManager = true
+
+
+    //preference
+    private lateinit var settingsDataStore: SettingsDataStore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +44,13 @@ class LetterListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         recyclerView = binding.recyclerView
         chooseLayout()
+
+        settingsDataStore = SettingsDataStore(requireContext())
+        settingsDataStore.preferenceFlow.asLiveData().observe(viewLifecycleOwner) { value ->
+            isLinearLayoutManager = value
+            chooseLayout()
+            activity?.invalidateOptionsMenu()
+        }
     }
 
     override fun onDestroyView() {
@@ -81,6 +96,10 @@ class LetterListFragment : Fragment() {
                 isLinearLayoutManager = !isLinearLayoutManager
                 chooseLayout()
                 setIcon(item)
+
+                lifecycleScope.launch{
+                    settingsDataStore.saveLayoutToPreferencesStore(isLinearLayoutManager, requireContext())
+                }
 
                 return true
             }
